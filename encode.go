7ecac1814ad3f636aa57defb272f07846e2cb6aa
ekzeb <ekzeboff@gmail.com>
2017-03-25 11:08:14 +0300

    fix tplt funcs

--- encode.go
+++ encode.go
@@ -1,1 +1,85 @@
 package util
+
+import (
+	"log"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"bytes"
+	"encoding/gob"
+)
+
+
+// store gob data
+func StoreGob(data interface{}, filename string, fileMode os.FileMode) (err error) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err = encoder.Encode(data)
+
+	if err != nil {
+		log.Println("Error Encode GOB data:", err)
+	}
+
+	err = ioutil.WriteFile(filename, buffer.Bytes(), fileMode)
+
+	if err != nil {
+		log.Println("Error Store GOB data:", err)
+	}
+
+	return
+}
+// store json data
+func StoreJson(data interface{}, filename string, fileMode os.FileMode) (err error)  {
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		log.Println("Error Marshal JSON data:", err)
+
+	}
+
+	if err := ioutil.WriteFile(filename, b, fileMode); err != nil {
+		log.Println("Error write JSON data:", err)
+
+	}
+
+	return
+}
+// load gob data
+func LoadGob(data interface{}, filename string) (err error) {
+	raw, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Println("Error Load GOB data:", err)
+	}
+
+	buffer := bytes.NewBuffer(raw)
+
+	dec := gob.NewDecoder(buffer)
+
+	err = dec.Decode(data)
+	if err != nil {
+		log.Println("Error Decode GOD data:", err)
+	}
+
+	return
+}
+// load json data
+func LoadJson(data interface{}, filename string) (err error) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Println("Error opening JSON file:", err)
+	}
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		log.Println("Error reading JSON data:", err)
+	}
+
+	if err := json.Unmarshal(jsonData, data); err != nil {
+		log.Println("Error Unmarshal JSON data:", err)
+	}
+
+	return
+}
--- time.go
+++ time.go
@@ -1,1 +1,29 @@
 package util
+
+import (
+	"time"
+	"strconv"
+)
+
+func NowLocalAsMs() int64 {
+	return TimeToMs(time.Now())
+}
+
+func NowUTCAsMs() int64 {
+	return TimeToMs(time.Now().UTC())
+}
+
+func TimeToMs(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+func MsToTime(ms string) (time.Time, error) {
+
+	msInt, err := strconv.ParseInt(ms, 10, 64)
+
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(0, msInt*int64(time.Millisecond)), nil
+}
